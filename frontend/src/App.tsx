eadaabc5981d6a7377dab3b783bf755bb710f7cc
Eder Vieira <eder.edy@gmail.com>
2022-07-13 22:30:06 -0400

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NoticationButton";
+
 function App() {
   return (
-    <h1>Olá mudo!</h1>
-  )
+    <>
+      <h1>Olá mudo!</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
